630357fa8a94629953e2682d8a1d78931f51cf14
Antoine Duprat <aduprat@linagora.com>
2018-11-19 12:45:52 +0100

    Custom configuration

--- src/test/scala-2.11/org/apache/james/gatling/jmap/scenari/common/Configuration.scala
+++ src/test/scala-2.11/org/apache/james/gatling/jmap/scenari/common/Configuration.scala
@@ -6,13 +6,13 @@ import java.net.URL
 object Configuration {
 
   val ServerHostName = "127.0.0.1"
-  val BaseJmapUrl = s"http://$ServerHostName:1080"
+  val BaseJmapUrl = s"http://$ServerHostName:80"
   val BaseJamesWebAdministrationUrl = new URL(s"http://$ServerHostName:8000")
 
-  val ScenarioDuration = 3 hours
-  val UserCount = 1000
-  val RandomlySentMails = 10
-  val NumberOfMailboxes = 10
-  val NumberOfMessages = 20
+  val ScenarioDuration = 1 hours
+  val UserCount = 300
+  val RandomlySentMails = 50
+  val NumberOfMailboxes = 20
+  val NumberOfMessages = 100
 
 }
